011d4a3c5a74d4a79b29ba4b874b2ccbab41cb33
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-01-26 16:36:38 -0700

    Javadoc, Enable/Disable support.

--- src/model/ProductQuantity.java
+++ src/model/ProductQuantity.java
@@ -2,69 +2,116 @@ package model;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-/**
- * Maintain a quantity (volume) along with an enumerated unit.
+/** Maintain a quantity (volume) along with an enumerated unit.
+ * 
+ * @author Matt Matheson
+ * @version 1.0 - Snell 340 Group 4 Phase 1
+ * 
  */
 public class ProductQuantity {
-	
-	/** Non-negative value for quantity. If units are count, must be integer.  */
+
+	// Member variables
 	private float quantity;
-	
-	/** Enumerated unit of measure for this quantity. */
 	private Unit units;
 	
-	/**
-	 * ProductQuantity constructor, sets quantity and unit.
-	 * @param q
-	 * @param u
+	/** Constructor
+	 * @param q Quantity, must be non-negative. If Unit is COUNT, must be integer.
+	 * @param u Unit
 	 */
 	public ProductQuantity(float q, Unit u){
 		throw new NotImplementedException();
 	}
 	
-	/**
-	 * getter for quantity
-	 * @return
+	/** Checks if float q and Unit u can be combined to create a valid
+	 * Product Group. q must be non-negative. If u is COUNT, q must be
+	 * an integer.
+	 * 
+	 * @param q float quantity to test.
+	 * @param u Unit to test.
+	 * @return true if q, u can be combined to make a valid ProductQuantity,
+	 * false otherwise 
+	 * 
+	 */
+	public static boolean isValidProductQuantity(float q, Unit u){
+		if(q < 0){
+			return false;
+		}
+		
+		if(u == Unit.COUNT && q != Math.round(q)){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/** Attribute getter for quantity
+	 * 
+	 * @return The float quantity associated with this ProductQuantity
 	 */
 	public float getQuantity(){
 		return this.quantity;
 	}
 	
-	/**
-	 * assign quantity
-	 * @param q - float if unit is not COUNT, integer otherwise
+	/** Attribute setter for quantity. This setter enforces that if the units
+	 * are COUNT, the quantity must be
+	 *  
+	 * @param q Float if unit is not COUNT, integer otherwise
+	 * @throws IllegalArgumentException If Unit is COUNT and q is not an integer 
 	 */
-	public void setQuantity(float q){
+	public void setQuantity(float q) throws IllegalArgumentException{
+		if(this.units == Unit.COUNT && q != Math.round(q)){
+			throw new IllegalArgumentException("Units are COUNT so quantity must be integer. Given " + q);
+		}
 		this.quantity = q;
 	}
 	
-	/**
-	 * get units associated with this quantity
-	 * @return
+	/** Attribute getter for Unit associated with this quantity
+	 * 
+	 * @return Unit Enum of this ProductQuantity
+	 * 
 	 */
 	public Unit getUnits(){
 		return this.units;
 	}
 	
-	/**
-	 * Add two quantities. Must have matching units.
-	 * @param quantityToAdd - ProductQuantity to add to this ProductQuantity, must have non-negative quantity
+	/** Adds a ProductQuantity to this ProductQuantity.
+	 * To add two ProductQuantities, their Unit must match.
+	 * 
+	 * @param quantityToAdd - ProductQuantity to add to this ProductQuantity
+	 * 
+	 * @throws IllegalArgumentException If this ProductQuantity's Unit does not
+	 * match incoming ProductQuantity Unit
+	 * 
 	 */
-	public void add(ProductQuantity quantityToAdd){
+	public void add(ProductQuantity quantityToAdd) throws IllegalArgumentException{
 		if(quantityToAdd.units != this.units){
 			throw new IllegalArgumentException("Cannot add quantities with different units!");
 		}
 		throw new NotImplementedException();
 	}
 	
-	/**
-	 * Subtract two quantities. Must have matching units, result must be non-negative.
-	 * @param quantityToSubtract - ProductQuantity to subtract from this ProductQuantity, must have non-negative quantity 
+	/** Subtracts a ProductQuantity from this ProductQuantity. The two ProductQuantities
+	 * Must have matching units, and the result result must be non-negative.
+	 * 
+	 * @param quantityToSubtract - ProductQuantity to subtract from this ProductQuantity, must have non-negative quantity
+	 *  
+	 * @throws IllegalArgumentException If this ProductQuantity's Unit does not
+	 * match incoming ProductQuantity Unit
 	 */
 	public void subtract(ProductQuantity quantityToSubtract){
 		if(quantityToSubtract.units != this.units){
 			throw new IllegalArgumentException("Cannot subtract quantities with different units!");
 		}
 		throw new NotImplementedException();
+	}
+	
+	
+	/** Gets a legible string representation of this ProductQuantity 
+	 * in the format <quantity> <Unit>
+	 * 
+	 * @return String representation of ProductQuantity
+	 */
+	public String toString(){
+		return this.quantity + " " + this.units.toString();
 	}
 }
