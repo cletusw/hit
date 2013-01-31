1ae3355878adcbff702887cd38848671cbd31570
Clayton <cletusw@gmail.com>
2013-01-31 10:46:05 -0700

    First tests for ProductQuantity

---
+++ unit-test-src/model/ProductQuantityTest.java
@@ -0,0 +1,96 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Clayton Watts
+ *
+ */
+public class ProductQuantityTest {
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#ProductQuantity(float, model.Unit)}.
+	 */
+	@Test
+	public void testProductQuantity() {
+		ProductQuantity pq = new ProductQuantity(3.2f, Unit.FLUID_OUNCES);
+		assertTrue(pq.getQuantity() == 3.2f);
+		assertTrue(pq.getUnits() == Unit.FLUID_OUNCES);
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#isValidProductQuantity(float, model.Unit)}.
+	 */
+	@Test
+	public void testIsValidProductQuantity() {
+		assertTrue(ProductQuantity.isValidProductQuantity(3.2f, Unit.FLUID_OUNCES));
+		assertTrue(ProductQuantity.isValidProductQuantity(0f, Unit.FLUID_OUNCES));
+		assertFalse(ProductQuantity.isValidProductQuantity(-1.4f, Unit.FLUID_OUNCES));
+		assertTrue(ProductQuantity.isValidProductQuantity(3, Unit.COUNT));
+		assertFalse(ProductQuantity.isValidProductQuantity(3.1f, Unit.COUNT));
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#setQuantity(float)}.
+	 */
+	@Test
+	public void testSetQuantity() {
+		ProductQuantity pq = new ProductQuantity(3.2f, Unit.FLUID_OUNCES);
+		pq.setQuantity(0f);
+		assertTrue(pq.getQuantity() == 0f);
+		
+		pq = new ProductQuantity(0, Unit.COUNT);
+		pq.setQuantity(1);
+		assertTrue(pq.getQuantity() == 1);
+		
+		thrown.expect(IllegalArgumentException.class);
+		pq.setQuantity(1.2f);
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#add(model.ProductQuantity)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#subtract(model.ProductQuantity)}.
+	 */
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.ProductQuantity#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
