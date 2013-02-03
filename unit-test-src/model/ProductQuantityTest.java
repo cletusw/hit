efbd43e6ec578122bfde1890023224324003767b
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-02-02 20:45:48 -0700

    ProductQuantity test add/subtract

--- unit-test-src/model/ProductQuantityTest.java
+++ unit-test-src/model/ProductQuantityTest.java
@@ -71,23 +71,144 @@ public class ProductQuantityTest {
 		thrown.expect(IllegalArgumentException.class);
 		count.setQuantity(1.2f);
 	}
+	
+	/**
+	 * Test method for {@link model.ProductQuantity#add(model.ProductQuantity)}.
+	 */
+	@Test
+	public void testValidAddCount() {
+		count.add(new ProductQuantity(4.0f, Unit.COUNT));
+		assertTrue(count.getQuantity() == 4f);
+		assertTrue(count.getUnits().equals(Unit.COUNT));
+	}
 
 	/**
 	 * Test method for {@link model.ProductQuantity#add(model.ProductQuantity)}.
 	 */
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testValidAddVolume() {
+		fluid.add(new ProductQuantity(1.0f, Unit.GALLONS));
+		assertTrue(fluid.getQuantity() == 131.2f);
+		assertTrue(fluid.getUnits().equals(Unit.FLUID_OUNCES));
+		
+		fluid.add(new ProductQuantity(3.3f, Unit.LITERS));
+		assertTrue(fluid.getQuantity() == 242.7862f);
+		assertTrue(fluid.getUnits().equals(Unit.FLUID_OUNCES));
+		
+		fluid.add(new ProductQuantity(2.0f, Unit.PINTS));
+		assertTrue(fluid.getQuantity() == 274.7862f);
+		assertTrue(fluid.getUnits().equals(Unit.FLUID_OUNCES));
+		
+		fluid.add(new ProductQuantity(3.0f, Unit.QUARTS));
+		assertTrue(fluid.getQuantity() == 370.7862f);
+		assertTrue(fluid.getUnits().equals(Unit.FLUID_OUNCES));
+	}
+	
+	/**
+	 * Test method for {@link model.ProductQuantity#add(model.ProductQuantity)}.
+	 */
+	@Test
+	public void testValidAddWeight() {
+		ProductQuantity kilos = new ProductQuantity(2.1f, Unit.KILOGRAMS);
+		kilos.add(new ProductQuantity(1.1f, Unit.GRAMS));
+		assert(kilos.getQuantity() == 2.1011f);
+		assert(kilos.getUnits().equals(Unit.KILOGRAMS));
+		
+		kilos.add(new ProductQuantity(2.3f, Unit.POUNDS));
+		assertTrue(kilos.getQuantity() == 3.1443615f);
+		assertTrue(kilos.getUnits().equals(Unit.KILOGRAMS));
+		
+		kilos.add(new ProductQuantity(12.2f, Unit.OUNCES));
+		assertTrue(kilos.getQuantity() == 3.4902253f);
+		assertTrue(kilos.getUnits().equals(Unit.KILOGRAMS));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidAddWeight(){
+		fluid.add(new ProductQuantity(1.0f, Unit.KILOGRAMS));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidAddCount(){
+		fluid.add(new ProductQuantity(1.0f, Unit.COUNT));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidAddVolume(){
+		count.add(new ProductQuantity(1.0f, Unit.FLUID_OUNCES));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidAddToWeight(){
+		ProductQuantity weight = new ProductQuantity(2.0f, Unit.KILOGRAMS);
+		weight.add(new ProductQuantity(3.0f, Unit.FLUID_OUNCES));
 	}
 
 	/**
 	 * Test method for {@link model.ProductQuantity#subtract(model.ProductQuantity)}.
 	 */
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
-	}
+	public void testValidSubtractVolume() {
+		ProductQuantity gallons = new ProductQuantity(3.0f, Unit.GALLONS);
+		gallons.subtract(fluid);
+		assertTrue(gallons.getQuantity() == 2.975f);
+		assertTrue(gallons.getUnits().equals(Unit.GALLONS));
 
+		gallons.subtract(new ProductQuantity(0.5f, Unit.LITERS));
+		assertTrue(gallons.getQuantity() == 2.8429139f);
+		assertTrue(gallons.getUnits().equals(Unit.GALLONS));
+		
+		gallons.subtract(new ProductQuantity(0.5f, Unit.PINTS));
+		assertTrue(gallons.getQuantity() == 2.7804139f);
+		assertTrue(gallons.getUnits().equals(Unit.GALLONS));
+		
+		gallons.subtract(new ProductQuantity(0.5f, Unit.QUARTS));
+		assertTrue(gallons.getQuantity() == 2.6554139f);
+		assertTrue(gallons.getUnits().equals(Unit.GALLONS));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidSubtractCount(){
+		fluid.subtract(new ProductQuantity(1.0f, Unit.COUNT));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidSubtractVolume(){
+		count.subtract(new ProductQuantity(1.0f, Unit.FLUID_OUNCES));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testInvalidSubtractToWeight(){
+		ProductQuantity weight = new ProductQuantity(2.0f, Unit.KILOGRAMS);
+		weight.subtract(new ProductQuantity(3.0f, Unit.FLUID_OUNCES));
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testNegativeInvalidSubtract(){
+		ProductQuantity weight = new ProductQuantity(2.0f, Unit.KILOGRAMS);
+		weight.subtract(new ProductQuantity(3.0f, Unit.KILOGRAMS));
+	}
+	
+	/**
+	 * Test method for {@link model.ProductQuantity#subtract(model.ProductQuantity)}.
+	 */
+	@Test
+	public void testValidSubtractWeight() {
+		ProductQuantity kilos = new ProductQuantity(3.0f, Unit.KILOGRAMS);
+
+		kilos.subtract(new ProductQuantity(0.5f, Unit.POUNDS));
+		assertTrue(kilos.getQuantity() == 2.773204f);
+		assertTrue(kilos.getUnits().equals(Unit.KILOGRAMS));
+		
+		kilos.subtract(new ProductQuantity(100f, Unit.GRAMS));
+		assertTrue(kilos.getQuantity() == 2.6732042f);
+		assertTrue(kilos.getUnits().equals(Unit.KILOGRAMS));
+		
+		kilos.subtract(new ProductQuantity(20f, Unit.OUNCES));
+		assertTrue(kilos.getQuantity() == 2.106214f);
+		assertTrue(kilos.getUnits().equals(Unit.KILOGRAMS));
+	}
+	
 	/**
 	 * Test method for {@link model.ProductQuantity#toString()}.
 	 */
