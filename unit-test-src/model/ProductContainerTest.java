6d911b7e9b4a402c797fcf90475e2cf118292a1a
Seth <Seth@blackbook.rn.byu.edu>
2013-02-04 23:19:26 -0700

    Adding test cases for ProductContainer

--- unit-test-src/model/ProductContainerTest.java
+++ unit-test-src/model/ProductContainerTest.java
@@ -9,10 +9,18 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ProductContainerTest {
-
+	static ProductContainer testStorageUnit1;
+	static ProductContainer testProductGroup1;
+	static Product product1;
+	static Product product2;
+	static Item item1;
+	static Item item2;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		
+		testStorageUnit1 = new StorageUnit("CupboardTest", Unit.COUNT);
+		testProductGroup1 = new ProductGroup("Cookies", new ProductQuantity(0, Unit.COUNT), Unit.KILOGRAMS);
+		//product1 = new Product("PROD1BARCODE", "Smelly socks", 0, int tms, ProductQuantity pq, ProductManager manager) {);
 	}
 
 	@AfterClass
@@ -23,7 +31,9 @@ public class ProductContainerTest {
 	@Test
 	public void PGtestItems() {
 		System.out.print("Testing ProductGroup Item logic...");
-		/*Item one = new Item("item1");
+		//Product product1 = new Product
+		/*
+		Item one = new Item(new Barcode("001", "item1", null);
 		Item two = new Item("item2");
 		Item oneCopy = new Item("item1");
 		ProductGroup pg1 = new ProductGroup("pg1",null);
@@ -37,8 +47,8 @@ public class ProductContainerTest {
 		assertEquals(1,pg1.getItemsSize());
 
 		pg1.clearAllItems();
-		assertEquals(0,pg1.getItemsSize());*/
-		
+		assertEquals(0,pg1.getItemsSize());
+		*/
 		System.out.println("done.");
 	}
 
@@ -92,5 +102,141 @@ public class ProductContainerTest {
 		System.out.print("Testing StorageUnit ProductGroup logic...");
 		System.out.println("done.");
 	}
+	
+	@Test
+	public void testProductContainer() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testGetUnit() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testGetItemsSize() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testGetProductsSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPGroupsSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProductGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentSupplyProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentSupply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProductGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanAddProductGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItemItemManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveIntoContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveProductContainer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveProductGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAllItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAllProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAllProductGroups() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}	
 }
 
