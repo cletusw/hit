4b421618951cc32a41f8cab63e3c786e129a7212
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-02-13 17:37:46 -0700

    Rename ConcreteStorageUnitManager to ConcreteProductContainerManager

--- unit-test-src/model/ConcreteProductContainerManagerTest.java
+++ unit-test-src/model/ConcreteProductContainerManagerTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ConcreteStorageUnitManagerTest {
+public class ConcreteProductContainerManagerTest {
 
 	@Before
 	public void setUp() throws Exception {
