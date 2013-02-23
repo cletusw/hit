2c4d1e578ce1854e56bb933150b5869870974bf4
Seth <Seth@blackbook.app.byu.edu>
2013-02-22 21:14:42 -0700

    Merge with InventoryController - minor change to include an ItemManager

--- src/gui/inventory/InventoryController.java
+++ src/gui/inventory/InventoryController.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import java.util.Set;
 
 import model.Item;
+import model.ItemManager;
 import model.Product;
 import model.ProductContainer;
 import model.ProductContainerManager;
