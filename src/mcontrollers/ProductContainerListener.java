3964bae2e19ec3801453f4a632eaac0cd1fc41db
mgh14 <chasing314@gmail.com>
2013-02-22 23:07:19 -0700

    Listeners parameter update

--- src/mcontrollers/ProductContainerListener.java
+++ src/mcontrollers/ProductContainerListener.java
@@ -32,7 +32,7 @@ public class ProductContainerListener implements Observer {
 	 * Method intended to notify the view when StorageUnitManager sends a "change" notice.
 	 * 
 	 * @param o
-	 *            The
+	 *            The object being observed (which has access to the model)
 	 * @param arg
 	 *            Object passed to the view so the view can determine which changes to make
 	 * 
