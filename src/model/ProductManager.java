2941a5d7e7693804b6bd698a1fbb54b4163e0c6f
Matthew <mmatheson01@gmail.com>
2013-03-06 20:18:09 -0700

    Removed editProductGroup, added notifyObservers(Action)

--- src/model/ProductManager.java
+++ src/model/ProductManager.java
@@ -12,9 +12,6 @@ public interface ProductManager {
 
 	public boolean containsProduct(String productName);
 
-	public void editProduct(Product product, String newDescription,
-			ProductQuantity newQuantity, int newShelfLife, int newTms);
-
 	public Product getByBarcode(String barcodeScanned);
 
 	public Set<Product> getProducts();
@@ -24,4 +21,6 @@ public interface ProductManager {
 	public void unmanage(Product product);
 
 	void addObserver(Observer o);
+	
+	public void notifyObservers(Action action);
 }
