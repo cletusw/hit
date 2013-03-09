8e2a01e602256c1ad3ce1b3e10bb9296b7631aa1
Clayton <cletusw@gmail.com>
2013-03-08 20:11:31 -0700

    Add Command JavaDocs

--- src/model/Command.java
+++ src/model/Command.java
@@ -1,7 +1,18 @@
 package model;
 
+/**
+ * Class that encapsulates an undo-able action.
+ * 
+ * @author clayton
+ */
 public interface Command {
+	/**
+	 * Executes the action that this Command represents.
+	 */
 	public void execute();
 
+	/**
+	 * Executes the inverse of the action that this Command represents.
+	 */
 	public void unexecute();
 }
