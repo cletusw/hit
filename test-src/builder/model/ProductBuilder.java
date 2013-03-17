d3347efcceb5615eace735fad5d5204bc02b3432
Clayton <cletusw@gmail.com>
2013-03-16 18:28:56 -0600

    Add setter methods to ProductBuilder

--- test-src/builder/model/ProductBuilder.java
+++ test-src/builder/model/ProductBuilder.java
@@ -17,8 +17,32 @@ public class ProductBuilder {
 	public ProductBuilder() {
 	}
 
+	public void barcode(String barcode) {
+		this.barcode = barcode;
+	}
+
 	public Product build() {
 		return new Product(barcode, description, shelfLife, threeMonthSupply, productQuantity,
 				productManager);
+	}
+
+	public void description(String description) {
+		this.description = description;
+	}
+
+	public void productManager(ProductManager productManager) {
+		this.productManager = productManager;
+	}
+
+	public void productQuantity(ProductQuantity productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+
+	public void shelfLife(int shelfLife) {
+		this.shelfLife = shelfLife;
+	}
+
+	public void threeMonthSupply(int threeMonthSupply) {
+		this.threeMonthSupply = threeMonthSupply;
 	}
 }
