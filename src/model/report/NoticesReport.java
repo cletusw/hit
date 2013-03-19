c9fb2fcaa8954d388b0328a35027af2c83172193
Matthew <mmatheson01@gmail.com>
2013-03-18 23:14:08 -0600

    NoticesReport implemented

--- src/model/report/NoticesReport.java
+++ src/model/report/NoticesReport.java
@@ -1,14 +1,27 @@
 package model.report;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
 import model.Item;
 import model.Product;
 import model.ProductContainer;
 import model.ProductContainerManager;
+import model.ProductGroup;
+import model.ProductQuantity;
+import model.StorageUnit;
+import model.Unit;
 import model.report.builder.ReportBuilder;
 import model.visitor.InventoryVisitor;
 
 @SuppressWarnings("serial")
 public class NoticesReport extends Report implements InventoryVisitor {
+	private ReportBuilder builder;
+	private ProductContainerManager pcManager;
+
 	/**
 	 * Set up an empty NoticesReport.
 	 * 
@@ -19,6 +32,8 @@ public class NoticesReport extends Report implements InventoryVisitor {
 	 * @post true
 	 */
 	public NoticesReport(ProductContainerManager productContainerManager) {
+		pcManager = productContainerManager;
+		reportName = "notices";
 	}
 
 	/**
@@ -31,6 +46,26 @@ public class NoticesReport extends Report implements InventoryVisitor {
 	 * @post (new Date()).getTime() - getLastRunTime().getTime() < 1000
 	 */
 	public void construct(ReportBuilder builder) {
+		updateLastRunTime();
+
+		// Store the builder for use by the Visitor pattern
+		this.builder = builder;
+
+		builder.addDocumentTitle("Notices");
+		builder.addSectionTitle("3-Month Supply Warnings");
+
+		for (StorageUnit su : pcManager.getStorageUnits()) {
+			su.accept(this);
+		}
+
+		try {
+			File file = builder.print(getFileName());
+			java.awt.Desktop.getDesktop().open(file);
+		} catch (IOException e) {
+			System.out.println("Not able to open!! " + getFileName());
+		}
+
+		this.builder = null;
 	}
 
 	@Override
@@ -43,5 +78,31 @@ public class NoticesReport extends Report implements InventoryVisitor {
 
 	@Override
 	public void visit(ProductContainer productContainer) {
+		if (productContainer instanceof ProductGroup) {
+			ProductGroup group = (ProductGroup) productContainer;
+			ProductQuantity groupSupply = group.getThreeMonthSupply();
+			Collection<Product> products = group.getProducts();
+			Map<String, Product> inconsistencies = new TreeMap<String, Product>();
+			for (Product product : products) {
+				if (Unit.typeMap.get(product.getSize().getUnits()) != Unit.typeMap
+						.get(groupSupply.getUnits())) {
+					inconsistencies.put(product.getDescription(), product);
+				}
+			}
+
+			if (inconsistencies.size() > 0) {
+				builder.startList("Product Group " + group.getRoot().getName() + "::"
+						+ group.getName() + " Has a 3-month supply (" + groupSupply
+						+ ") that is inconsistent with the following products:");
+				Collection<String> descriptions = inconsistencies.keySet();
+				for (String d : descriptions) {
+					Product p = inconsistencies.get(d);
+					builder.addListItem(group.getName() + "::" + p.getDescription()
+							+ "(size: " + p.getSize().toString() + ")");
+				}
+
+			}
+		}
+
 	}
 }
