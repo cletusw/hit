6407f9f16520e545c1bf9f189ba5b9e2d7c43c09
Matthew <mmatheson01@gmail.com>
2013-03-30 21:27:16 -0600

    model.persistence.factory javadoc

--- src/model/persistence/factory/RdbDaoFactory.java
+++ src/model/persistence/factory/RdbDaoFactory.java
@@ -2,6 +2,14 @@ package model.persistence.factory;
 
 import model.persistence.InventoryDao;
 
+/**
+ * Creates and maintains an RdbDao object to be used for persistence.
+ * 
+ * @author Matthew
+ * @version 1.0 -- Snell CS 340 Phase 4.0
+ * 
+ * @invariant InventoryDao != null
+ */
 public class RdbDaoFactory implements DaoFactory {
 
 	@Override
--- src/model/persistence/factory/SerializationDaoFactory.java
+++ src/model/persistence/factory/SerializationDaoFactory.java
@@ -2,6 +2,14 @@ package model.persistence.factory;
 
 import model.persistence.InventoryDao;
 
+/**
+ * Creates and maintains a SerializationDao object to be used for persistence.
+ * 
+ * @author Matthew
+ * @version 1.0 -- Snell CS 340 Phase 4.0
+ * 
+ * @invariant InventoryDao != null
+ */
 public class SerializationDaoFactory implements DaoFactory {
 
 	@Override
