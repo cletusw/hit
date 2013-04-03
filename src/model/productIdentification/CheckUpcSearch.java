ef6fbcac7ce998097cd4f43086d4392972a0399f
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-04-03 12:26:16 -0600

    CheckUpcSearch added

--- src/model/productIdentification/CheckUpcSearch.java
+++ src/model/productIdentification/CheckUpcSearch.java
@@ -1,7 +1,8 @@
 package model.productIdentification;
 
-public class CheckUpcSearch extends ProductIdentificationPlugin {
+public class CheckUpcSearch implements ProductIdentificationPlugin {
 
+	// Example: http://www.checkupc.com/product-54091
 	@Override
 	public String getDescriptionForProduct(String productBarcode) {
 		// TODO Auto-generated method stub
