7a56604e6e002239b6c8e6e9001118fa88b8f6ba
Seth <pygosceles@yahoo.com>
2013-04-05 14:29:43 -0600

    Added JUnit test for UpcDatabaseApi

---
+++ test-src/test/model/productIdentification/UpcDatabaseApiTest.java
@@ -0,0 +1,31 @@
+package test.model.productIdentification;
+
+import static org.junit.Assert.assertTrue;
+import model.productIdentification.UpcDatabaseApi;
+
+import org.easymock.EasyMockSupport;
+import org.junit.Test;
+
+import common.util.HttpClient;
+import common.util.IHttpClient;
+
+public class UpcDatabaseApiTest extends EasyMockSupport {
+
+	@Test
+	public void testGetDescriptionForProduct() {
+		// IHttpClient c = createMock(IHttpClient.class);
+		// String response = expect(c.getHttpRequest((String)
+		// EasyMock.notNull())).andStubReturn(response);
+
+		IHttpClient client = new HttpClient();
+
+		replayAll();
+
+		UpcDatabaseApi api = new UpcDatabaseApi(client);
+		String desc = api.getDescriptionForProduct("035000053640");
+		assertTrue(desc != null);
+		assertTrue(desc.equals("Ajax with Bleach Poweder Cleaner"));
+
+		verifyAll();
+	}
+}
