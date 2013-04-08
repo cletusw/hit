f4c7eadfa6f583bcc52165d4aad7f80799f7aeee
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-04-08 17:45:52 -0600

    Barcode Scanner implemented

--- src/gui/batches/RemoveItemBatchController.java
+++ src/gui/batches/RemoveItemBatchController.java
@@ -6,9 +6,11 @@ import gui.common.IView;
 import gui.item.ItemData;
 import gui.product.ProductData;
 
-import java.util.Set;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.TreeMap;
 
 import model.Action;
@@ -26,10 +28,28 @@ import model.undo.UndoManager;
  */
 public class RemoveItemBatchController extends Controller implements
 		IRemoveItemBatchController {
-	
+
+	/**
+	 * Task runs when barcode scanner is enabled
+	 * 
+	 * @author Matthew
+	 * 
+	 */
+	private class RemoveItemTask extends TimerTask {
+
+		@Override
+		public void run() {
+			if (getView().getUseScanner() && getView().getBarcode().length() > 0)
+				removeItem();
+		}
+	}
+
+	private Timer timer;
+	private RemoveItemTask removeItemTask;
+
 	private UndoManager manager;
 	private Map<Product, ArrayList<Item>> removedItems;
-	private Map<Item,String> itemsPCnames;
+	private Map<Item, String> itemsPCnames;
 
 	/**
 	 * Constructor.
@@ -39,14 +59,46 @@ public class RemoveItemBatchController extends Controller implements
 	 */
 	public RemoveItemBatchController(IView view) {
 		super(view);
-
+		getView().setUseScanner(true);
+		timer = new Timer();
+		removeItemTask = new RemoveItemTask();
 		manager = new UndoManager();
-		
+
 		removedItems = new TreeMap<Product, ArrayList<Item>>();
-		
+
 		itemsPCnames = new TreeMap<Item, String>();
-		
+
 		construct();
+	}
+
+	/**
+	 * Method that adds an Item to the removedItems map
+	 * 
+	 * @param item
+	 *            the item that was just removed
+	 * @param itemContainer
+	 *            the removed item's previous container
+	 * 
+	 * @pre item != null
+	 * @pre itemContainer != null
+	 * @post removedItems.get(item.getProduct()) != null
+	 * @post if(removedItems.get(item.getProduct()@pre != null)
+	 *       removedItems.get(item.getProduct()@post == removedItems.get(item.getProduct()@pre
+	 *       + 1
+	 */
+	public void addItemToRemoved(Item item, ProductContainer itemContainer) {
+		if (item == null)
+			throw new IllegalArgumentException("Item shouldn't be null");
+
+		if (removedItems.get(item.getProduct()) == null)
+			removedItems.put(item.getProduct(), new ArrayList<Item>());
+
+		ArrayList<Item> removedByProduct = removedItems.get(item.getProduct());
+		itemsPCnames.put(item, itemContainer.getName());
+		if (removedByProduct.contains(item))
+			throw new IllegalStateException("RemovedItems already contains this item");
+
+		removedByProduct.add(item);
 	}
 
 	/**
@@ -55,6 +107,8 @@ public class RemoveItemBatchController extends Controller implements
 	 */
 	@Override
 	public void barcodeChanged() {
+		if (getView().getUseScanner())
+			setTimer();
 		enableComponents();
 	}
 
@@ -77,14 +131,14 @@ public class RemoveItemBatchController extends Controller implements
 	 */
 	@Override
 	public void redo() {
-		if(!manager.canRedo())
+		if (!manager.canRedo())
 			throw new IllegalStateException("No redo action available");
-		
+
 		RemoveItem command = (RemoveItem) manager.redo();
-		
-		((ConcreteItemManager) getItemManager()).notifyObservers(
-				new Action(command.getRemovedItem(),ActionType.DELETE));
-		
+
+		((ConcreteItemManager) getItemManager()).notifyObservers(new Action(command
+				.getRemovedItem(), ActionType.DELETE));
+
 		enableComponents();
 	}
 
@@ -102,14 +156,50 @@ public class RemoveItemBatchController extends Controller implements
 			view.setBarcode("");
 			return;
 		}
-		
+
 		Item item = getItemManager().getItemByItemBarcode(view.getBarcode());
-		
-		manager.execute(new RemoveItem(view.getBarcode(),getItemManager(),this));
-		
-		((ConcreteItemManager) getItemManager()).notifyObservers(new Action(item,ActionType.DELETE));
-		
+
+		manager.execute(new RemoveItem(view.getBarcode(), getItemManager(), this));
+
+		((ConcreteItemManager) getItemManager()).notifyObservers(new Action(item,
+				ActionType.DELETE));
+
 		enableComponents();
+	}
+
+	/**
+	 * Method that adds an Item to the removedItems map
+	 * 
+	 * @param item
+	 *            the item that was just removed
+	 * @param itemContainer
+	 *            the removed item's previous container
+	 * 
+	 * @pre item != null
+	 * @pre itemContainer != null
+	 * @post removedItems.get(item.getProduct()) != null
+	 * @post if(removedItems.get(item.getProduct()@pre != null)
+	 *       removedItems.get(item.getProduct()@post == removedItems.get(item.getProduct()@pre
+	 *       + 1
+	 */
+	public void removeItemFromRemoved(Item item) {
+		if (item == null)
+			throw new IllegalArgumentException("Item shouldn't be null");
+
+		Product itemProduct = item.getProduct();
+
+		if (removedItems.get(itemProduct) == null)
+			removedItems.put(itemProduct, new ArrayList<Item>());
+
+		ArrayList<Item> removedByProduct = removedItems.get(itemProduct);
+		itemsPCnames.remove(item);
+		if (!removedByProduct.contains(item))
+			throw new IllegalStateException("Removed items doesn't contain this item");
+
+		removedByProduct.remove(item);
+
+		if (removedByProduct.isEmpty())
+			removedItems.remove(itemProduct);
 	}
 
 	/**
@@ -123,16 +213,16 @@ public class RemoveItemBatchController extends Controller implements
 		ProductData selected = view.getSelectedProduct();
 		if (selected == null)
 			return;
-		
+
 		Product selectedProduct = (Product) selected.getTag();
-		
+
 		ArrayList<Item> productItems = removedItems.get(selectedProduct);
-		if(productItems == null || productItems.isEmpty())
+		if (productItems == null || productItems.isEmpty())
 			throw new IllegalStateException("This product should have item(s)");
-		
+
 		ItemData[] items = new ItemData[productItems.size()];
 		int counter = 0;
-		for(Item item : productItems) {
+		for (Item item : productItems) {
 			items[counter] = DataWrapper.wrap(item);
 			items[counter++].setStorageUnit(itemsPCnames.get(item));
 		}
@@ -149,15 +239,44 @@ public class RemoveItemBatchController extends Controller implements
 	 */
 	@Override
 	public void undo() {
-		if(!manager.canUndo())
+		if (!manager.canUndo())
 			throw new IllegalStateException("No undo action available");
-		
+
 		RemoveItem command = (RemoveItem) manager.undo();
-		
-		((ConcreteItemManager) getItemManager()).notifyObservers(
-				new Action(command.getRemovedItem(),ActionType.DELETE));
-		
+
+		((ConcreteItemManager) getItemManager()).notifyObservers(new Action(command
+				.getRemovedItem(), ActionType.DELETE));
+
 		enableComponents();
+	}
+
+	/**
+	 * Method that updates the view after an execute() or redo() call
+	 * 
+	 * @param removedItem
+	 *            the item just removed
+	 * 
+	 * @pre removedItem != null
+	 * @post true
+	 */
+	public void updateOnExecuteOrRedo(Item removedItem) {
+		if (removedItem == null)
+			throw new IllegalArgumentException("removedItem can't be null");
+
+		updateRemovedProducts(removedItem);
+	}
+
+	/**
+	 * Method that updates the view after an Undo call
+	 * 
+	 * @param removedItem
+	 *            the Item's removal to undo
+	 * 
+	 * @pre removedItem != null
+	 * @post true
+	 */
+	public void updateOnUndo(Item removedItem) {
+		updateRemovedProducts(removedItem);
 	}
 
 	/**
@@ -166,6 +285,30 @@ public class RemoveItemBatchController extends Controller implements
 	 */
 	@Override
 	public void useScannerChanged() {
+	}
+
+	private void setTimer() {
+		removeItemTask.cancel();
+		removeItemTask = new RemoveItemTask();
+		timer.schedule(removeItemTask, 700l);
+	}
+
+	private void updateRemovedProducts(Item item) {
+		assert (item != null);
+
+		// update product view
+		Set<Product> keys = removedItems.keySet();
+		ProductData[] products = new ProductData[keys.size()];
+		int counter = 0;
+		for (Product product : keys) {
+			products[counter++] = DataWrapper.wrap(product, removedItems.get(product).size());
+		}
+		getView().setProducts(products);
+
+		// update item view
+		getView().setItems(new ItemData[0]);
+
+		enableComponents();
 	}
 
 	/**
@@ -180,7 +323,7 @@ public class RemoveItemBatchController extends Controller implements
 	@Override
 	protected void enableComponents() {
 		getView().enableItemAction(!getView().getBarcode().equals(""));
-		
+
 		getView().enableRedo(manager.canRedo());
 		getView().enableUndo(manager.canUndo());
 	}
@@ -203,111 +346,5 @@ public class RemoveItemBatchController extends Controller implements
 	@Override
 	protected void loadValues() {
 		// no code needed - no pre-loaded state for this batch
-	}
-	
-	/**
-	 * Method that updates the view after an execute() or redo() call
-	 * 
-	 * @param removedItem the item just removed
-	 * 
-	 * @pre removedItem != null
-	 * @post true
-	 */
-	public void updateOnExecuteOrRedo(Item removedItem) {
-		if(removedItem == null)
-			throw new IllegalArgumentException("removedItem can't be null");
-		
-		updateRemovedProducts(removedItem);
-	}
-	
-	/**
-	 * Method that adds an Item to the removedItems map
-	 * 
-	 * @param item the item that was just removed
-	 * @param itemContainer the removed item's previous container
-	 * 
-	 * @pre item != null
-	 * @pre itemContainer != null
-	 * @post removedItems.get(item.getProduct()) != null
-	 * @post if(removedItems.get(item.getProduct()@pre != null)
-	 * 		removedItems.get(item.getProduct()@post == 
-	 * 		removedItems.get(item.getProduct()@pre + 1
-	 */
-	public void addItemToRemoved(Item item,ProductContainer itemContainer) {
-		if(item == null)
-			throw new IllegalArgumentException("Item shouldn't be null");
-		
-		if(removedItems.get(item.getProduct()) == null)
-			removedItems.put(item.getProduct(), new ArrayList<Item>());
-		
-		ArrayList<Item> removedByProduct = removedItems.get(item.getProduct());
-		itemsPCnames.put(item, itemContainer.getName());
-		if(removedByProduct.contains(item))
-			throw new IllegalStateException("RemovedItems already contains this item");
-		
-		removedByProduct.add(item);
-	}
-
-	/**
-	 * Method that adds an Item to the removedItems map
-	 * 
-	 * @param item the item that was just removed
-	 * @param itemContainer the removed item's previous container
-	 * 
-	 * @pre item != null
-	 * @pre itemContainer != null
-	 * @post removedItems.get(item.getProduct()) != null
-	 * @post if(removedItems.get(item.getProduct()@pre != null)
-	 * 		removedItems.get(item.getProduct()@post == 
-	 * 		removedItems.get(item.getProduct()@pre + 1
-	 */
-	public void removeItemFromRemoved(Item item) {
-		if(item == null)
-			throw new IllegalArgumentException("Item shouldn't be null");
-		
-		Product itemProduct = item.getProduct();
-		
-		if(removedItems.get(itemProduct) == null)
-			removedItems.put(itemProduct, new ArrayList<Item>());
-		
-		ArrayList<Item> removedByProduct = removedItems.get(itemProduct);
-		itemsPCnames.remove(item);
-		if(!removedByProduct.contains(item))
-			throw new IllegalStateException("Removed items doesn't contain this item");
-		
-		removedByProduct.remove(item);
-		
-		if(removedByProduct.isEmpty())
-			removedItems.remove(itemProduct);
-	}
-		
-	/**
-	 * Method that updates the view after an Undo call
-	 * 
-	 * @param removedItem the Item's removal to undo
-	 * 
-	 * @pre removedItem != null
-	 * @post true
-	 */
-	public void updateOnUndo(Item removedItem) {
-		updateRemovedProducts(removedItem);
-	}
-	
-	private void updateRemovedProducts(Item item) {
-		assert(item != null);
-		
-		// update product view
-		Set<Product> keys = removedItems.keySet();
-		ProductData[] products = new ProductData[keys.size()];
-		int counter = 0;
-		for(Product product : keys) {
-			products[counter++] = DataWrapper.wrap(product, removedItems.get(product).size());
-		}
-		getView().setProducts(products);
-
-		// update item view
-		getView().setItems(new ItemData[0]);
-		
-		enableComponents();
 	}
 }
