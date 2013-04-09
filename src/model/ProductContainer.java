88e0f1d4df8be6247bc34d84fcd5e385e0b9e4df
Matthew Matheson <mmatheson@yellowpagesdir.com>
2013-04-08 19:35:22 -0600

    Drag item mostly fixed

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -859,6 +859,7 @@ public abstract class ProductContainer implements Comparable<ProductContainer>, 
 	 */
 	protected void registerItem(Item i) {
 		items.put(i.getBarcode(), i);
+		products.put(i.getProductBarcode(), i.getProduct());
 		i.setContainer(this);
 		i.getProduct().addItem(i);
 		Set<Item> newItemsForProduct;
