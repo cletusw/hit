e19a0143b19f060baadfeceec5a57fe6a673e89d
Clayton <cletusw@gmail.com>
2013-04-08 22:49:54 -0600

    UpcDatabaseApiTest uses manager

--- test-src/test/model/productIdentification/UpcDatabaseApiTest.java
+++ test-src/test/model/productIdentification/UpcDatabaseApiTest.java
@@ -8,22 +8,23 @@ import org.easymock.EasyMockSupport;
 import org.junit.Test;
 
 import plugin.IHttpClient;
-import plugin.UpcDatabaseApi;
+import plugin.ProductIdentificationPluginManager;
 
 public class UpcDatabaseApiTest extends EasyMockSupport {
 
 	@Test
 	public void testGetDescriptionForProduct() {
-		IHttpClient client = createMock(IHttpClient.class);
+		IHttpClient httpClient = createMock(IHttpClient.class);
+		ProductIdentificationPluginManager manager = new ProductIdentificationPluginManager(
+				"plugin.UpcDatabaseApi", httpClient);
 		String response = "<html><body><table>"
 				+ "<tr><td>Description</td><td></td><td>Ajax</td></tr></table>"
 				+ "</body></html>";
-		expect(client.getHttpRequest((String) EasyMock.notNull())).andReturn(response);
+		expect(httpClient.getHttpRequest((String) EasyMock.notNull())).andReturn(response);
 
 		replayAll();
 
-		UpcDatabaseApi api = new UpcDatabaseApi(client);
-		String desc = api.getDescriptionForProduct("035000053640");
+		String desc = manager.getDescriptionForProduct("035000053640");
 		assertTrue(desc != null);
 		assertTrue(desc.equals("Ajax"));
 
