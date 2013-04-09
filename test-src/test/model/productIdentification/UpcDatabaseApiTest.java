84357f33f9f3aab62a8b8eb61a363a21861fd124
Clayton <cletusw@gmail.com>
2013-04-08 22:48:27 -0600

    Refactor UpcDatabaseApiTest

--- test-src/test/model/productIdentification/UpcDatabaseApiTest.java
+++ test-src/test/model/productIdentification/UpcDatabaseApiTest.java
@@ -1,31 +1,31 @@
 package test.model.productIdentification;
 
+import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.assertTrue;
 
+import org.easymock.EasyMock;
 import org.easymock.EasyMockSupport;
 import org.junit.Test;
 
-import plugin.HttpClient;
 import plugin.IHttpClient;
 import plugin.UpcDatabaseApi;
-
 
 public class UpcDatabaseApiTest extends EasyMockSupport {
 
 	@Test
 	public void testGetDescriptionForProduct() {
-		// IHttpClient c = createMock(IHttpClient.class);
-		// String response = expect(c.getHttpRequest((String)
-		// EasyMock.notNull())).andStubReturn(response);
-
-		IHttpClient client = new HttpClient();
+		IHttpClient client = createMock(IHttpClient.class);
+		String response = "<html><body><table>"
+				+ "<tr><td>Description</td><td></td><td>Ajax</td></tr></table>"
+				+ "</body></html>";
+		expect(client.getHttpRequest((String) EasyMock.notNull())).andReturn(response);
 
 		replayAll();
 
 		UpcDatabaseApi api = new UpcDatabaseApi(client);
 		String desc = api.getDescriptionForProduct("035000053640");
 		assertTrue(desc != null);
-		assertTrue(desc.equals("Ajax with Bleach Poweder Cleaner"));
+		assertTrue(desc.equals("Ajax"));
 
 		verifyAll();
 	}
